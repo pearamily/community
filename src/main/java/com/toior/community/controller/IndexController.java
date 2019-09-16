252e2dfca21a14acbb106bb0b90a03511f1b38d0
pearamily <pearamily@outlook.com>
2019-09-16 12:43:48 +0800

    add flyway support

--- src/main/java/com/toior/community/controller/IndexController.java
+++ src/main/java/com/toior/community/controller/IndexController.java
@@ -1,15 +1,48 @@
 package com.toior.community.controller;
 
+import com.toior.community.mapper.UserMapper;
+import com.toior.community.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(/*@RequestParam(name = "name") String name, Model model*/) {
+    public String index(/*@RequestParam(name = "name") String name, Model model*/HttpServletRequest request) {
+
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies.length <= 1) {
+            return "index";
+        }
+
+        for (Cookie cookie :
+                cookies) {
+            if ("token".equals(cookie.getName())) {
+                String token = cookie.getValue();
+                if ("".equals(token) || token == null) {
+                    break;
+                }
+                User user = userMapper.findByToken(token);
+
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
+
         return "index";
     }
 }
